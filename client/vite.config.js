a1bf58f96811c8371f18243bc8b80961216c0b83
Manikandaprabhu07 <prabhumani7112005@gmail.com>
2026-04-02 14:50:48 +0530

    new backed url

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'https://porlour-booking.onrender.com',
+        target: 'https://parlour-booking.onrender.com',
         changeOrigin: true,
       },
     },
